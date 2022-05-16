c870219f8d1843ad7546b8a4102c384400d48c60
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-16 13:35:00 +0300

    set the module exports as an object

--- src/Controllers/Event.controller.js
+++ src/Controllers/Event.controller.js
@@ -1,23 +1,24 @@
 const Event = require('../Models/Event.model');
 
-const add = () => {
-    const event = Event.build({
-        numEvent: 'E_11022022',
-        title: 'event1',
-        category: 'drame',
-        categoryAge: 'Enfant',
-        cost: 100,
-        dateEvent: new Date('11-02-2022')
-    });
+module.exports = {
 
-    event.save()
-        .then(() => {
-            console.log('added successfully')
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    ;
+    add: () => {
+        const event = Event.build({
+            numEvent: 'E_11022022',
+            title: 'event1',
+            category: 'drame',
+            categoryAge: 'Enfant',
+            cost: 100,
+            dateEvent: new Date('11-02-2022')
+        });
+    
+        event.save()
+            .then(() => {
+                console.log('added successfully')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        ;
+    }
 }
-
-module.exports = add;
