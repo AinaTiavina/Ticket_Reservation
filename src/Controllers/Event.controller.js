353d304a7d178e1c5583d88d88ca0a0ea42c337c
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-16 18:56:00 +0300

    fetch data and create event methods

--- src/Controllers/Event.controller.js
+++ src/Controllers/Event.controller.js
@@ -2,23 +2,26 @@ const Event = require('../Models/Event.model');
 
 module.exports = {
 
-    add: () => {
-        const event = Event.build({
-            numEvent: 'E_11022022',
-            title: 'event1',
-            category: 'drame',
-            categoryAge: 'Enfant',
-            cost: 100,
-            dateEvent: new Date('11-02-2022')
-        });
-    
-        event.save()
-            .then(() => {
-                console.log('added successfully')
-            })
-            .catch(err => {
-                console.log(err);
-            })
-        ;
+    getAllEvents: (req, res, next) => {
+        Event.findAll()
+            .then( events => res.status(200).json({ events }))
+            .catch( err => res.status(400).json({ err }))
+    },
+
+    createEvent: (req, res, next) => {
+        Event.create({
+            numEvent: "E_"+req.body.dateEvent,
+            title: req.body.title,
+            category: req.body.category,
+            categoryAge: req.body.categoryAge,
+            cost: req.body.cost,
+            dateEvent: new Date(req.body.dateEvent)
+        })
+        .then( event => {
+            return res.status(201).json({event})   
+        })
+        .catch(err => {
+            return res.status(400).json({err});
+        })
     }
 }
--- src/Router/Event.route.js
+++ src/Router/Event.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const EventController = require('../Controllers/Event.controller');
 const router = express.Router();
 
-router.get('/')
-router.post('/')
-router.get('/:id')
-router.put('/:id')
-router.delete('/:id')
+router.get('/', EventController.getAllEvents);
+router.post('/', EventController.createEvent);
+router.get('/:id');
+router.put('/:id');
+router.delete('/:id');
 
 module.exports = router;
