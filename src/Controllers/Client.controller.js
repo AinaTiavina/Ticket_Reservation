4000d0796f1a70183daf6724f71d63496b616cd3
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-16 22:48:15 +0300

    create method for getting back all users

---
+++ src/Controllers/Client.controller.js
@@ -0,0 +1,14 @@
+const client = require('../Models/Client.model');
+
+module.exports = {
+
+    retrieveAllClients: (req, res, next) => {
+        client.findAll({
+            attributes: {
+                exclude: 'id'
+            }
+        })
+        .then( clients => res.status(200).json(clients))
+        .catch( err => res.status(400).json(err))
+    }
+}
