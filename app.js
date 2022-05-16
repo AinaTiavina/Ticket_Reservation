d66a97739c402117b47f5ede3cac72983416b09a
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-16 22:49:16 +0300

    setup base route

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router = require('./src/Router/Event.route');
+const eventRouter = require('./src/Router/Event.route');
+const clientRouter = require('./src/Router/Client.route');
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/events', router)
+app.use('/api/events', eventRouter);
+app.use('/api/clients', clientRouter);
 
 module.exports = app;
