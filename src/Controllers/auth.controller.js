3859e8c69a83dbee2ec9917ad3aee77907b0b03f
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-17 21:28:28 +0300

    add authentication handler

--- src/Controllers/auth.controller.js
+++ src/Controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const client = require('../Models/Client.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../Config/auth.config');
 
 module.exports = {
 
@@ -27,5 +29,31 @@ module.exports = {
         .catch( err => res.status(500).json({
             error: err.message
         }));
+    },
+
+    authentication: (req, res, next) => {
+        
+        client.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then( user => {
+            bcrypt.compare(req.body.password, user.password)
+                .then( valid => {
+                    if(!valid){
+                        return res.status(401).json({
+                            message: "Incorrect information."
+                        });
+                    }
+
+                    return res.status(200).json({
+                        email: user.email,
+                        token: jwt.sign({ id: user.id }, config.secret, {
+                            expiresIn: 10800            // 3 hours
+                        })
+                    })
+                })
+        })
     }
 }
