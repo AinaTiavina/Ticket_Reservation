e4466ad3f26e88d6f26ff823666c2d52c7b01781
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-18 22:21:05 +0300

    customize headers

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const { clientRoutes, eventRoutes, authenticationRoutes } = require('./src/Route
 const sequelize = require('./src/Services/database.service');
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 sequelize.sync({alter: false, force: false})
     .then(() => {
         console.log('Synchronized successfully');
