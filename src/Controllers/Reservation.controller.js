203be0f2154b4e1a3df66d38cc8151923b7c37eb
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-19 20:04:24 +0300

    store the reservation

--- src/Controllers/Reservation.controller.js
+++ src/Controllers/Reservation.controller.js
@@ -1,4 +1,6 @@
-const { reservation } = require('../Models');
+const { reservation, event, client } = require('../Models');
+const jwt = require('jsonwebtoken');
+const config = require('../Config/auth.config');
 
 module.exports = {
 
@@ -10,5 +12,29 @@ module.exports = {
         })
         .then( reservations => res.status(200).json(reservations))
         .catch( err => res.status(500).json(err));
+    },
+
+    insertReservation: (req, res, next) => {
+        const token = req.headers['authorization'].split(' ')[1];
+
+        jwt.verify(token, config.secret, (err, decoded) => {
+            client.findByPk(decoded.id)
+            .then( user => {
+                event.findByPk(req.query.event)
+                    .then( event => {
+                        reservation.create({
+                            placeNumber: req.body.placeNumber,
+                            ClientId: user.id,
+                            EventId: event.id
+                        })
+                        .then( reservation => res.status(201).json(reservation) )
+                        .catch( err => res.status(400).json(err) );
+                    })
+                    .catch( () => res.status(400).json({
+                        message: 'Event not found.'
+                    }));    
+                })
+            .catch( err => res.status(400).json(err) )
+        })
     }
 }
--- src/Routes/reservation.routes.js
+++ src/Routes/reservation.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
 const { reservationController } = require("../Controllers");
+const { authorizationJwt } = require("../Middlewares");
 
 const router = Router();
 
 router.get('/', reservationController.fetchAllReservations);
-router.post('/');
+router.post(
+    '/', 
+    [
+        authorizationJwt.verifyToken
+    ],
+    reservationController.insertReservation
+);
 
 module.exports = router;
