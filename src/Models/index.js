17b42ea5adf115f2cb9cd2f1536ef036d4aa9bbe
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-19 18:19:52 +0300

    configure relations between tables

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,7 +1,27 @@
 const client = require('./Client.model');
 const event = require('./Event.model');
+const reservation = require('./Reservation.model');
+
+client.hasMany(reservation, {
+    foreignKey: {
+        foreignKey: 'clientId',
+        allowNull: false
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+});
+
+event.hasMany(reservation, {
+    foreignKey: {
+        foreignKey: 'eventId',
+        allowNull: false
+    },
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE'
+});
 
 module.exports = {
     client,
-    event
+    event,
+    reservation
 };
