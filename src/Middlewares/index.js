33166ccd165618b12dc61653bdc55965caa026c7
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-05-20 22:24:12 +0300

    configure multer for uploading image

--- src/Middlewares/index.js
+++ src/Middlewares/index.js
@@ -2,10 +2,12 @@ const verifyClientRegistration = require('./register');
 const loginVerification = require('./login');
 const authorizationJwt = require('./authJwt');
 const reservationChecking = require('./reservation');
+const file = require('./fileUpload');
 
 module.exports = {
     verifyClientRegistration,
     loginVerification,
     authorizationJwt,
-    reservationChecking
+    reservationChecking,
+    file
 };
