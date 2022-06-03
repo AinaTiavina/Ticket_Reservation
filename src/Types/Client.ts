6f67310cc916099fdb0953751e13e68245b710a6
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-03 17:12:41 +0300

    create client's type

---
+++ src/Types/Client.ts
@@ -0,0 +1,13 @@
+interface Client {
+    id: number,
+    codecli: string,
+    lastName: string,
+    firstName: string,
+    email: string,
+    roles: string | string[],
+    password: string,
+    phone: string,
+    cardNumber: string
+}
+
+export default Client;
