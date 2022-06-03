a38966c50839f976f54bbdaeba43e5e28786e7fa
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-03 20:24:27 +0300

    Event and Reservation types

---
+++ src/Types/Event.ts
@@ -0,0 +1,12 @@
+interface Event {
+    id: number,
+    numEvent: string,
+    title: string,
+    category: string,
+    categoryAge: string,
+    cost: number,
+    dateEvent: string,
+    imageUrl: string
+}
+
+export default Event;
---
+++ src/Types/Reservation.ts
@@ -0,0 +1,9 @@
+interface Reservation {
+    id: number,
+    codeReservation: string,
+    placeNumber: string,
+    payed: boolean,
+    reservationDate: string
+}
+
+export default Reservation;
