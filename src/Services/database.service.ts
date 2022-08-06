c404bbb291e8243004d094a2273a758e40105b4d
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-08-06 21:26:49 +0300

    db config done

--- src/Services/database.service.ts
+++ src/Services/database.service.ts
@@ -2,10 +2,23 @@ import { Sequelize as SequelizeType} from "sequelize/types";
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const env = process.env;
+let sequelize;
 
-const sequelize: SequelizeType = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
-    host: env.DB_HOST,
-    dialect: env.DB_DIALECT
-});
+if(process.env.NODE_ENV === "development") {
+    sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
+        host: env.DB_HOST,
+        dialect: env.DB_DIALECT
+    });
+} else {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      }
+    );
+}
 
 module.exports = sequelize;
