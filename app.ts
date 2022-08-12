8a77921cd3a6ccfedfdcef3e90eef0075803f4c8
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-08-12 19:03:42 +0300

    solve cors problems

--- app.ts
+++ app.ts
@@ -4,12 +4,13 @@ const { clientRoutes, eventRoutes, authenticationRoutes, reservationRoutes } = r
 const sequelize = require('./src/Services/database.service');
 const app = express();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            // access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // configure a static path
 app.use(express.static(__dirname+'/public'));
